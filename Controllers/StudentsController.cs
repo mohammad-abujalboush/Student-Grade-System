814ef1c6a764b42e66baaa419649e473897b6176
mohammad-abujalboush <mohammadabujalboush02@gmail.com>
2023-11-27 22:01:47 +0300

    first
    Date: 27/11/2023

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentGradeBookSystem.Models;
+using System.Diagnostics;
+
+
+namespace StudentGradeBookSystem.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+        public static List<Student> students = new List<Student>();
+
+        public ActionResult Index()
+        {
+                return View();            
+        }
+
+        // GET: StudentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student e)
+        {
+            if (ModelState.IsValid == true)
+            {
+                students.Add(e);
+            }
+            return View("ListOfStudents", students);
+        }
+
+        public ActionResult ListOfStudents(Student sg)
+        {
+            return View("ListOfStudents", students);
+        }
+
+        
+        public ActionResult Delete(string name)
+        {
+            
+            Student st = students.Find(x => x.Name == name);
+            students.Remove(st);
+
+            return View("ListOfStudents", students);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(string name)
+        {
+            
+            return View("ListOfStudents", students.Where(x => x.Name.Contains(name)));
+
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace StudentGradeBookSystem.Models
+{
+    public class Student
+    {
+        [Key]
+        [Required]
+        [MaxLength(9)]
+        [RegularExpression("[0-9]+")]
+        public string Id { get; set; }
+        [RegularExpression("[a-z A-Z]+")]
+        public string Name { get; set; }
+        [RegularExpression("[a-z A-Z0-9]+")]
+        public string CourseTitle { get; set; }
+        [Range(0, 30)]
+        public int? AssignmentGrade { get; set; }
+        [Range(0, 30)]       
+        public int? MidTermGrade { get; set; }
+        [Range(0, 40)]        
+        public int? FinalGrade { get; set; }
+        public double? Total { get { return AssignmentGrade + MidTermGrade + FinalGrade; } }
+
+
+    }
+}
